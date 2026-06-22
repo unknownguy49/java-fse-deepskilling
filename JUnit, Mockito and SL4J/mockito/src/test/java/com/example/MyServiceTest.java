551d2b4ad6ece30f1425d5d90e1def1c51433e05
Dibyadyuti Dutta <dibyadyutidutta49@gmail.com>
2026-06-22 21:15:47 +0530

    Mockito and SLF4J Logging added

---
+++ JUnit, Mockito and SL4J/mockito/src/test/java/com/example/MyServiceTest.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class MyServiceTest 
+{
+    @Test
+    public void testExternalApi() 
+    {
+        ExternalApi mockApi=Mockito.mock(ExternalApi.class);
+
+        when(mockApi.getData()).thenReturn("Mock Data");
+
+        MyService service=new MyService(mockApi);
+
+        String result=service.fetchData();
+
+        assertEquals("Mock Data",result);
+    }
+}
