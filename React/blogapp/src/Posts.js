4eaa908aef7502078fdc4e1ab1d162c6e8f297ba
Dibyadyuti Dutta <dibyadyutidutta49@gmail.com>
2026-07-12 19:00:50 +0530

    react mandatory 1-5 added

---
+++ React/blogapp/src/Posts.js
@@ -0,0 +1,48 @@
+import React from "react";
+import Post from "./Post";
+
+class Posts extends React.Component{
+
+    constructor(props){
+        super(props);
+
+        this.state={
+            posts:[]
+        };
+    }
+
+    loadPosts(){
+        fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(response=>response.json())
+        .then(data=>{
+            const posts=data.map(post=>new Post(post.id,post.title,post.body));
+            this.setState({posts});
+        });
+    }
+
+    componentDidMount(){
+        this.loadPosts();
+    }
+
+    componentDidCatch(error,info){
+        alert(error);
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>Blog Posts</h1>
+
+                {this.state.posts.map(post=>(
+                    <div key={post.id}>
+                        <h2>{post.title}</h2>
+                        <p>{post.body}</p>
+                        <hr/>
+                    </div>
+                ))}
+            </div>
+        );
+    }
+}
+
+export default Posts;
---
+++ React/scorecalculatorapp/src/App.js
@@ -0,0 +1,16 @@
+import { CalculateScore } from "./Components/CalculateScore";
+
+function App() {
+  return (
+    <div>
+      <CalculateScore
+        Name={"Dibyadyuti"}
+        School={"Adamas International School"}
+        total={549}
+        goal={6}
+      />
+    </div>
+  );
+}
+
+export default App;
